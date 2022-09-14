2bc866154db2c4d60ad45ef9342795dd57b1d0b8
AlexandreGuyonnetWCS <alexandre.guyonnet.pro@gmail.com>
2022-09-14 16:53:44 +0200

    add show for product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Product;
 use App\Entity\Category;
 use App\Entity\ParentCategory;
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -28,12 +29,18 @@ class ProductController extends AbstractController
     #[ParamConverter('product', options: ['mapping' => ['product' => 'slug']])]
     #[ParamConverter('category', options: ['mapping' => ['category' => 'slug']])]
     #[ParamConverter('parentCategory', options: ['mapping' => ['parentCategory' => 'slug']])]
-    public function showProduct(Product $product, Category $category, ParentCategory $parentCategory): Response
-    {
+    public function showProduct(
+        Product $product,
+        Category $category,
+        ParentCategory $parentCategory,
+        ProductRepository $productRepository
+    ): Response {
+        $products = $productRepository->findByCategory($category);
         return $this->render('product/show.html.twig', [
             'product' => $product,
             'category' => $category,
             'parentCategory' => $parentCategory,
+            'products' => $products,
         ]);
     }
 }
