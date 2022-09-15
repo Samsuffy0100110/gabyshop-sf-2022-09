18a0673a1a44be12662555388e9b4ce10f1335c1
AlexandreGuyonnetWCS <alexandre.guyonnet.pro@gmail.com>
2022-09-15 18:02:09 +0200

    add bdd contents in all footer includes + add socials entity + admin

---
+++ src/Controller/Admin/SocialCrudController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Social;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class SocialCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Social::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return Crud::new()
+            ->setEntityLabelInSingular('Réseau social')
+            ->setEntityLabelInPlural('Réseaux sociaux')
+            ->setSearchFields(['id', 'name', 'link'])
+            ->setDefaultSort(['id' => 'DESC'])
+            ->setPageTitle('index', 'Administration des réseaux sociaux');
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            ChoiceField::new('name')
+                ->setLabel('Nom')
+                ->setChoices([
+                    'Facebook' => 'facebook',
+                    'Instagram' => 'instagram',
+                    'Twitter' => 'twitter',
+                    'Pinterest' => 'pinterest',
+                    'Youtube' => 'youtube',
+                    'Linkedin' => 'linkedin',
+                    'TikTok' => 'tiktok',
+                    'Snapchat' => 'snapchat',
+                    'Twitch' => 'twitch',
+                    'Discord' => 'discord',
+                    'Autre' => 'arobase',
+                ]),
+            TextField::new('link')
+                ->setLabel('Lien'),
+        ];
+    }
+}
