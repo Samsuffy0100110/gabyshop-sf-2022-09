66aaf870f5464754a0bad71a3c5afb0de84abef4
AlexandreGuyonnetWCS <alexandre.guyonnet.pro@gmail.com>
2022-09-17 16:30:28 +0200

    add EventSuscriber for shops and theme error stan + fixe front in product

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Front;
 
+use App\Repository\Front\ThemeRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
---
+++ src/EventSubscriber/TwigEventSubscriber.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Twig\Environment;
+use App\Repository\Front\ShopRepository;
+use App\Repository\Front\ThemeRepository;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\ControllerEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class TwigEventSubscriber implements EventSubscriberInterface
+{
+    private $twig;
+
+    private $themeRepository;
+
+    private $shopRepository;
+
+    public function __construct(Environment $twig, ThemeRepository $themeRepository, ShopRepository $shopRepository)
+    {
+        $this->twig = $twig;
+        $this->themeRepository = $themeRepository;
+        $this->shopRepository = $shopRepository;
+    }
+
+    public function onControllerEvent(ControllerEvent $event)
+    {
+        $this->twig->addGlobal('theme', $this->themeRepository->findOneBy(['isActive' => true]));
+        $this->twig->addGlobal('shops', $this->shopRepository->findAll());
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::CONTROLLER => 'onControllerEvent',
+        ];
+    }
+}
