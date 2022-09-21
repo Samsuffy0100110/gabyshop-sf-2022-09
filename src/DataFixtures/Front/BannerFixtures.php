821ec8fc73fa8b4381f3d187e25ff5da4cae6180
Yoann Brambilla <yobrambilla@gmail.com>
2022-09-21 13:08:58 +0200

    add banners datas

--- src/DataFixtures/Front/BannerFixtures.php
+++ src/DataFixtures/Front/BannerFixtures.php
@@ -12,8 +12,8 @@ class BannerFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         $banner = new Banner();
-        $banner->setName("Bannière 1");
-        $banner->setImage("banner-test.jpg");
+        $banner->setName("-15 % pour Noël");
+        $banner->setImage("christmas.jpg");
         $banner->setPosition(1);
         $banner->setIsActive(true);
         $banner->setCreatedAt(new DateTime());
@@ -22,8 +22,8 @@ class BannerFixtures extends Fixture
         $manager->persist($banner);
 
         $banner = new Banner();
-        $banner->setName("Bannière 2");
-        $banner->setImage("https://loremflickr.com/300/300?random=2");
+        $banner->setName("Peur sur les prix !");
+        $banner->setImage("halloween.jpg");
         $banner->setPosition(2);
         $banner->setIsActive(true);
         $banner->setCreatedAt(new DateTime());
@@ -32,10 +32,10 @@ class BannerFixtures extends Fixture
         $manager->persist($banner);
 
         $banner = new Banner();
-        $banner->setName("Bannière 3");
-        $banner->setImage("https://loremflickr.com/300/300?random=3");
+        $banner->setName("Chassez les promos !");
+        $banner->setImage("easter.jpg");
         $banner->setPosition(3);
-        $banner->setIsActive(false);
+        $banner->setIsActive(true);
         $banner->setCreatedAt(new DateTime());
         $banner->setStartedAt(new DateTime());
         $banner->setEndedAt(new DateTime());
