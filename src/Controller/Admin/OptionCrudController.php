0b3bc015ab44b768ada2301bbea8780c238afc52
AlexandreGuyonnetWCS <alexandre.guyonnet.pro@gmail.com>
2022-09-26 16:05:08 +0200

    add option and parentoption entities and admin crud

---
+++ src/Controller/Admin/OptionCrudController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Product\Option;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+
+class OptionCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Option::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return Crud::new()
+            ->setEntityLabelInSingular('Option')
+            ->setEntityLabelInPlural('Options')
+            ->setPageTitle('index', 'Administration des Options')
+            ->setSearchFields(['id', 'name'])
+            ->setDefaultSort(['id' => 'DESC'])
+            ->setPaginatorPageSize(10);
+    }
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('name')
+                ->setLabel('Nom'),
+            AssociationField::new('optionParent', 'Catégorie d\'Option')
+                ->setLabel('Catégorie d\'Option'),
+        ];
+    }
+}
