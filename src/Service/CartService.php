21a8a997788b6faf5d86ec0ac7ce1a54de0453d8
Yoann Brambilla <yobrambilla@gmail.com>
2022-10-04 14:24:09 +0200

    test cart

--- src/Service/CartService.php
+++ src/Service/CartService.php
@@ -60,6 +60,7 @@ class CartService
                 // $product_object = $this->em->getRepository(Product::class)->findOneById($id);
                 // $attributs = $this->em->getRepository(Attribut::class)->findByProduct($product_object);
                 $attribut = $this->entityManager->getRepository(Attribut::class)->find($id);
+                $product = $this->entityManager->getRepository(Product::class)->find($attribut->getProduct());
                 // $products = $this->em->getRepository(Product::class)->find($attribut->getProduct());
                 if (!$attribut) {
                     $this->delete($id);
@@ -68,7 +69,7 @@ class CartService
                 $cartComplete[] = [
                     'attribut' => $attribut,
                     'quantity' => $quantity,
-
+                    'product' => $product,
                 ];
             }
         }
