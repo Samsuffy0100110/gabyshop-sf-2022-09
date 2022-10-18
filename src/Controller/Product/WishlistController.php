8ae66b72172f9d6ca7ce3f3f02263ae2d69efaf5
Yoann Brambilla <yobrambilla@gmail.com>
2022-10-18 17:31:51 +0200

    finish wishlist method

--- src/Controller/Product/WishlistController.php
+++ src/Controller/Product/WishlistController.php
@@ -2,29 +2,29 @@
 
 namespace App\Controller\Product;
 
+use App\Entity\Product\Product;
 use App\Entity\Product\Wishlist;
+use App\Repository\Product\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use App\Repository\Product\WishlistRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 #[Route('/wishlist')]
 class WishlistController extends AbstractController
 {
-    #[Route('/new', name: 'wishlist_new', methods: ['GET', 'POST'])]
-    public function new(WishlistRepository $wishlistRepository): Response
+    #[Route('/new/{product}', name: 'wishlist_new', methods: ['GET', 'POST'])]
+    #[ParamConverter('product', options: ['mapping' => ['product' => 'slug']])]
+    public function new(Product $product, WishlistRepository $wishlistRepository): Response
     {
         $wishlist = new Wishlist();
         $wishlist->setUser($this->getUser());
-        // $wishlist->setProduct($wishlistRepository->find($request->get('product')));
+        $wishlist->setProduct($product);
         $wishlistRepository->save($wishlist, true);
 
         return $this->redirectToRoute('account_wishlist');
-
-        return $this->renderForm('product/_wishlist.html.twig', [
-            'wishlist' => $wishlist,
-        ]);
     }
 
     #[Route('/{id}', name: 'wishlist_delete', methods: ['POST'])]
