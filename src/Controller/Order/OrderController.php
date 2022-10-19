f7e84b9cd1aca931565ddb0f28bea372cbb5195f
Yoann Brambilla <yobrambilla@gmail.com>
2022-10-19 11:38:08 +0200

    change style on stripe

--- src/Controller/Order/OrderController.php
+++ src/Controller/Order/OrderController.php
@@ -109,6 +109,7 @@ class OrderController extends AbstractController
         Stripe\Charge::create([
             'amount' => $cart,
             "currency" => "eur",
+            'description' => 'Commande sur gabyShop',
             "source" => $request->request->get('stripeToken'),
         ]);
         $this->addFlash(
