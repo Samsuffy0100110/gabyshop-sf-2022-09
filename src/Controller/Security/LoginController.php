0659b8084d7e2aa570531c5108d508e639c42f21
samSuffy <yobrambilla@gmail.com>
2022-12-08 11:12:50 +0100

    add logicaction on logout

--- src/Controller/Security/LoginController.php
+++ src/Controller/Security/LoginController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller\Security;
 
+use App\Service\CartService;
+use App\Repository\Product\CustomRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
@@ -25,5 +28,21 @@ class LoginController extends AbstractController
     {
         // controller can be blank: it will never be called!
         // throw new Exception('Don\'t forget to activate logout in security.yaml');
+    }
+
+    #[Route('/logoutAction', name: 'logoutAction', methods: ['GET'])]
+    public function logoutAction(
+        CustomRepository $customRepository
+    ): Response {
+        $customs = $customRepository->findBy(['customOrder' => null]);
+        foreach ($customs as $custom) {
+            $customRepository->createQueryBuilder('c')
+                ->delete()
+                ->where('c.id = :id')
+                ->setParameter('id', $custom->getId())
+                ->getQuery()
+                ->execute();
+        }
+        return $this->redirectToRoute('home');
     }
 }
